cf950578b32abe1c5573f4686cc5cc19d7567982
Tsvetov1984 <acvetov68@gmail.com>
2024-04-29 18:59:55 +0300

    ASD-8

--- include/train.h
+++ include/train.h
@@ -3,18 +3,29 @@
 #define INCLUDE_TRAIN_H_
 
 class Train {
- private:
-  struct Cage {
-    bool light; // состояние лампочки
-    Cage *next;
-    Cage *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Cage *first; // точка входа в поезд (первый вагон)
- public:
-  Train();
-  void addCage(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+private:
+    struct Cage {
+        bool light; // состояние лампочки
+        Cage *next;
+        Cage *prev;
+    };
+
+    Train::Cage *create(bool light) {
+        Train::Cage *wagon = new Train::Cage;
+        wagon->prev = nullptr;
+        wagon->next = nullptr;
+        wagon->light = light;
+        return wagon;
+    }
+
+    int countOp; // счетчик шагов (число переходов из вагона в вагон)
+    Cage *first; // точка входа в поезд (первый вагон)
+public:
+    Train();
+
+    void addCage(bool light); // добавить вагон с начальным состоянием лампочки
+    int getLength();          // вычислить длину поезда
+    int getOpCount();         // вернуть число переходов (из вагона в вагон)
 };
+
 #endif  // INCLUDE_TRAIN_H_
--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,62 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+void Train::addCage(bool light) {
+    Train::Cage *wagon = create(light);
+    if (first == nullptr) {
+        first = wagon;
+    } else {
+        if (first->next == nullptr) {
+            first->prev = wagon;
+            first->next = wagon;
+            wagon->next = first;
+            wagon->prev = first;
+
+        } else {
+            Train::Cage *temp = first;
+            while (temp->next != first) {
+                temp = temp->next;
+            }
+            temp->next = wagon;
+            wagon->prev = temp;
+            wagon->next = first;
+            first->prev = wagon;
+        }
+    }
+    countOp = 0;
+}
+
+Train::Train() : first(nullptr), countOp(0) {}
+
+int Train::getLength() {
+    Train::Cage *CurrentWagon = first;
+    if (CurrentWagon->light == false) {
+        CurrentWagon->light = true;
+    }
+    int rez = 0;
+    int countWagon = 0;
+    while (true) {
+        CurrentWagon = CurrentWagon->next;
+        countOp += 1;
+        countWagon += 1;
+        while (CurrentWagon->light == false) {
+            CurrentWagon = CurrentWagon->next;
+            countOp += 1;
+            countWagon += 1;
+        }
+        CurrentWagon->light = false;
+        rez = countWagon;
+        while (countWagon != 0) {
+            countOp += 1;
+            countWagon -= 1;
+            CurrentWagon = CurrentWagon->prev;
+        }
+        if (CurrentWagon->light == false) {
+            return rez;
+        }
+    }
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
